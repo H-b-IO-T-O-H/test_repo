056de94a3446b59eaf77c85670e7ec37429888d3
Amelin Vladislav <vamelin@ozon.ru>
2021-09-15 17:11:06 +0300

    fix tests

--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def test_elem_sum(self):
         a, b = 2, 3
         output = elem_sum(a, b)
-        self.assertEqual(output, 1)
+        self.assertEqual(output, 5)
 
     def test(self):
         self.assertEqual(True, True)
